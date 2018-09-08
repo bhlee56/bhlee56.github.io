a4239b0a39e89dc0db64cb8070454803d1eaf520
bhlee56 <42564040+bhlee56@users.noreply.github.com>
2018-09-08 19:28:28 +0900

    Update swipe.js

--- swipe.js
+++ swipe.js
@@ -5,8 +5,12 @@ function swipedetect(el, callback){
   startX,
   startY,
   distX,
-  threshold = screen.width, //required min distance traveled to be considered swipe
-  restraint = screen.height, // maximum distance allowed at the same time in perpendicular direction
+  distY,
+  threshold = 40, //required min distance traveled to be considered swipe
+  restraint = 30, // maximum distance allowed at the same time in perpendicular direction
+  allowedTime = 300, // maximum time allowed to travel that distance
+  elapsedTime,
+  startTime,
   handleswipe = callback || function(swipedir){}
 
   touchsurface.addEventListener('touchstart', function(e){
@@ -15,25 +19,19 @@ function swipedetect(el, callback){
     dist = 0
     startX = touchobj.pageX
     startY = touchobj.pageY
+    startTime = new Date().getTime() // record time when finger first makes contact with surface
   }, false)
 
-  touchsurface.addEventListener('touchmove', function(e){
-    var touchobj = e.changedTouches[0]
-    distX = touchobj.pageX - startX
-
-    var element = document.getElementById('swipe')
-    element.style.transform = 'translate3d(' + distX + 'px,0,0)'
-  }, false)
+  touchsurface.addEventListener('touchmove', function(e){}, false)
 
   touchsurface.addEventListener('touchend', function(e){
     var touchobj = e.changedTouches[0]
-    distX = touchobj.pageX - startX // get total dist traveled by finger while in contact with surface
-    if (Math.abs(distX) >= threshold / 4 && Math.abs(touchobj.pageY - startY) <= restraint / 12){
-      swipedir = (distX < 0)? 'right' : 'left'
+    dist = touchobj.pageX - startX // get total dist traveled by finger while in contact with surface
+    elapsedTime = new Date().getTime() - startTime // get time elapsed
+    // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+    if (elapsedTime <= allowedTime && Math.abs(dist) >= threshold && Math.abs(touchobj.pageY - startY) <= restraint){
+      swipedir = (dist < 0)? 'right' : 'left'
     }
-
-    document.getElementById('swipe').setAttribute("style", "transform: translate3d(0,0,0); transition-duration: 0.3s;")
-
     handleswipe(swipedir)
   }, false)
 }
